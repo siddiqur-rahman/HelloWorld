513898d763e8a0f988f0e245928d53116a136a7a
siddiqur-rahman <siddiqur.rahman077@gmail.com>
2016-08-18 03:47:19 +0600

    added display greeting and fixed caps.

--- HelloWrold/src/Main.java
+++ HelloWrold/src/Main.java
@@ -2,9 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
+		
+		displayGreeting();
+		
 		System.out.println("hello world");
 		System.out.println("test1");
 		System.out.println("test2");
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This app shows sales data.");
+		System.out.println("----------------------------");
 	}
 
 }
